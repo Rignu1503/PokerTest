dcba9bafe3813ae75302f7d08f16b0ae0895996b
Rignu1503 <rigoberto.miranda1503@gmail.com>
2024-08-13 10:43:18 -0500

    request and response creation

---
+++ src/main/java/com/IESSoluciones/Prueba_2/api/dto/request/HandRequest.java
@@ -0,0 +1,14 @@
+package com.IESSoluciones.Prueba_2.api.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HandRequest {
+
+    private String hand1;
+    private String hand2;
+}
