519bbf9549588b23c533a9a737f8a69fc6ae26ac
Rignu1503 <rigoberto.miranda1503@gmail.com>
2024-08-14 08:57:35 -0500

    refactoring service hand

--- src/main/java/com/IESSoluciones/Prueba_2/infrastructure/factory/HandFactory.java
+++ src/main/java/com/IESSoluciones/Prueba_2/infrastructure/factory/HandFactory.java
@@ -2,40 +2,33 @@ package com.IESSoluciones.Prueba_2.infrastructure.factory;
 
 import com.IESSoluciones.Prueba_2.api.dto.request.HandRequest;
 import com.IESSoluciones.Prueba_2.api.dto.response.HandResponse;
-import com.IESSoluciones.Prueba_2.infrastructure.factory.interfaces.IhandFactory;
-import com.IESSoluciones.Prueba_2.infrastructure.service.AbstractService.IhandSerivce;
+import com.IESSoluciones.Prueba_2.infrastructure.factory.interfaces.IHandFactory;
+import com.IESSoluciones.Prueba_2.infrastructure.service.AbstractService.IHandSerivce;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @AllArgsConstructor
-public class HandFactory implements IhandFactory {
+public class HandFactory implements IHandFactory {
 
-    private final IhandSerivce handService;
-
+    private final IHandSerivce handService;
 
     @Override
     public HandResponse ValidatedHand(HandRequest request) {
+        
+        if(this.handService.validatedPoker(request) != null){
+            return this.handService.validatedPoker(request);
 
-        HandResponse pairResponse = handService.validatedPair(request);
-        HandResponse twoPairResponse = handService.validatedTwoPair(request);
-        HandResponse threeOfAKindResponse = handService.validatedWinThreeOfAKind(request);
-        HandResponse pokerResponse = handService.validatedPoker(request);
+        } else if (this.handService.validatedWinThreeOfAKind(request) != null){
+            return handService.validatedWinThreeOfAKind(request);
 
-        if(pokerResponse != null)
-            return pokerResponse;
-
-         else if (threeOfAKindResponse != null)
-            return threeOfAKindResponse;
-
-        else if (twoPairResponse != null)
-            return twoPairResponse;
-
-        else if (pairResponse != null) {
-            return pairResponse;
+         } else if (this.handService.validatedTwoPair(request) != null){
+            return this.handService.validatedTwoPair(request);
+            
+         }  else if (this.handService.validatedPair(request) != null) {
+            return this.handService.validatedPair(request);
         }
 
-        return handService.validatedHighCard(request);
+        return this.handService.validatedHighCard(request);
     }
 }
---
+++ src/main/java/com/IESSoluciones/Prueba_2/infrastructure/service/AbstractService/IHandSerivce.java
@@ -0,0 +1,4 @@
+package com.IESSoluciones.Prueba_2.infrastructure.service.AbstractService;
+
+public interface IHandSerivce extends WinHighCard, WinPair, WinTwoPair, WinThreeOfAKind,WinPoker{
+}
